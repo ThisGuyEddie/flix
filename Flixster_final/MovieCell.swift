2279c693a2cf9ba49affb3cec834f9932e4f2d99
Eddie Rodriguez <eddie.albert.rodriguez@gmail.com>
2019-02-14 16:46:22 -0800

    pushed from assignment 1
    before starting assignment 2

---
+++ Flixster_final/MovieCell.swift
@@ -0,0 +1,29 @@
+//
+//  MovieCell.swift
+//  Flixster_final
+//
+//  Created by Kevin Pham on 2/4/19.
+//  Copyright © 2019 Kevin Pham. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Podfile
@@ -0,0 +1,23 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Flixster_final' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Flixster_final
+  pod "AlamofireImage"
+
+
+
+  target 'Flixster_finalTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'Flixster_finalUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
